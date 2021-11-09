614df2a094a8b4bc6bef38584b5d8a4ee67ee5b2
ZQE <dev@zqe.io>
2021-11-09 20:04:45 +0600

    init

---
+++ includes/class-woo-variable-product-swatches-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/zqelab
+ * @since      1.0.0
+ *
+ * @package    Woo_Variable_Product_Swatches
+ * @subpackage Woo_Variable_Product_Swatches/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Woo_Variable_Product_Swatches
+ * @subpackage Woo_Variable_Product_Swatches/includes
+ * @author     ZQE <dev@zqe.io>
+ */
+class Woo_Variable_Product_Swatches_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
